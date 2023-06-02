d3e3019a230dffbae9ec2727667d7522e58fcad9
Paul Merizalde <merizaldepaul1999>
2023-06-01 19:40:54 -0500

    Taller 12

--- js/calculadoraWeb.js
+++ js/calculadoraWeb.js
@@ -2,10 +2,14 @@ var operacion = '';
 var num1 = '';
 var num2 = '';
 var decimal = 0;
-
+let bol = false;
 function guardarValor(valor) {
     decimal = valor == '.' ? decimal++ : decimal++;
-
+    if(bol){
+        reset();
+        document.getElementById('labelResultado').value =' ';
+        bol = false;
+    }
     if (decimal > 1 && valor == '.') valor = ''
     operacion == '' ? num1 += valor : num2 += valor;
     document.getElementById('labelResultado').value = parseFloat(operacion == '' ? num1 : num2);
@@ -13,6 +17,7 @@ function guardarValor(valor) {
 }
 
 function igual() {
+    bol = true
     num1 = parseFloat(num1)
     num2 = parseFloat(num2)
     switch (operacion) {
@@ -36,6 +41,12 @@ function igual() {
 }
 
 function guardarOperacion(op) {
-    operacion = op
+    operacion = op;
+}
+
+function reset(){
+    operacion = '';
+    num1=0
+    num2=0
 }
 
